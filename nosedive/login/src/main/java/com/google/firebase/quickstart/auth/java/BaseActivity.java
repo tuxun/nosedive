7fd7c1d1546ab44cd4613afcc38f204283d5c27e
tuxun <tuxun@tuxun.fr>
2020-10-22 02:57:41 +0200

    firebase WIP

---
+++ nosedive/login/src/main/java/com/google/firebase/quickstart/auth/java/BaseActivity.java
@@ -0,0 +1,43 @@
+package com.google.firebase.quickstart.auth.java;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import androidx.appcompat.app.AppCompatActivity;
+import org.tflsh.login.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+  public ProgressDialog mProgressDialog;
+
+  public void showProgressDialog() {
+    if (mProgressDialog == null) {
+      mProgressDialog = new ProgressDialog(this);
+      mProgressDialog.setMessage(getString(R.string.app_name));
+      mProgressDialog.setIndeterminate(true);
+    }
+
+    mProgressDialog.show();
+  }
+
+  public void hideProgressDialog() {
+    if (mProgressDialog != null && mProgressDialog.isShowing()) {
+      mProgressDialog.dismiss();
+    }
+  }
+
+  public void hideKeyboard(View view) {
+    final InputMethodManager imm =
+        (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    if (imm != null) {
+      imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    hideProgressDialog();
+  }
+}
